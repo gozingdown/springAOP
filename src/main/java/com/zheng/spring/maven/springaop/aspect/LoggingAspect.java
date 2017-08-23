e4ac0b4ae34670324f8c94eb00325edad5e192f6
gozingdown <gongzhenggz@gmail.com>
2017-08-22 22:52:28 -0400

    pointcuts

--- src/main/java/com/zheng/spring/maven/springaop/aspect/LoggingAspect.java
+++ src/main/java/com/zheng/spring/maven/springaop/aspect/LoggingAspect.java
@@ -2,17 +2,18 @@ package com.zheng.spring.maven.springaop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String com.zheng.spring.maven.springaop.model.Circle.getName())")
+	@Before("execution(public String com.zheng.spring.maven.springaop.model.Circle.get*(..))")
 	public void loggingAdvice() {
 		System.out.println("Advice run. Get Method called.");
 	}
 	
-	// wildcard
-	@Before("execution(public String get*(..))")
+	// pointcut
+	@Before("allGetters()")
 	public void loggingAdvice2() {
 		System.out.println("Advice2 run. Get Method called.");
 	}
@@ -28,4 +29,14 @@ public class LoggingAspect {
 //	public void loggingAdvice3() {
 //		System.out.println("Advice3 run. Get Method called.");
 //	}
+	
+	
+	@Before("allGetters()")
+	public void secondAdvice() {
+		System.out.println("Second Advice executed.");
+	}
+	
+	@Pointcut("execution(public String get*(..))")
+	public void allGetters() {}
+
 }
