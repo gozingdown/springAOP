f415241e24c99f2b47ef305273ea1c05de3b9891
Zheng Gong <zgong@vistaprint.com>
2017-08-25 10:58:41 -0400

    JoinPoints and Advice Arguments

--- src/main/java/com/zheng/spring/maven/springaop/AopMain.java
+++ src/main/java/com/zheng/spring/maven/springaop/AopMain.java
@@ -9,6 +9,7 @@ public class AopMain {
 	public static void main(String[] args) { 
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 		ShapeService shapeService = ctx.getBean("shapeService", ShapeService.class);
+		shapeService.getCircle().setName("Dummy Name");
 		System.out.println(shapeService.getCircle().getName());
 		System.out.println(shapeService.getTriangle().getName());
 	}
--- src/main/java/com/zheng/spring/maven/springaop/aspect/LoggingAspect.java
+++ src/main/java/com/zheng/spring/maven/springaop/aspect/LoggingAspect.java
@@ -1,21 +1,27 @@
 package com.zheng.spring.maven.springaop.aspect;
 
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
+import com.zheng.spring.maven.springaop.model.Circle;
+
 @Aspect
 public class LoggingAspect {
 	
-	@Before("execution(public String com.zheng.spring.maven.springaop.model.Circle.get*(..))")
-	public void loggingAdvice() {
-		System.out.println("Advice run. Get Method called.");
-	}
+//	@Before("execution(public String com.zheng.spring.maven.springaop.model.Circle.get*(..))")
+//	public void loggingAdvice() {
+//		System.out.println("Advice run. Get Method called.");
+//	}
 	
-	// pointcut
-	@Before("allGetters()")
-	public void loggingAdvice2() {
-		System.out.println("Advice2 run. Get Method called.");
+//	@Before("allGetters() && allCircleMethods()")
+	@Before("allGetters() && allCircleMethods()")
+	public void loggingAdvice(JoinPoint joinPoint) {
+		System.out.println("JoinPoint: " + joinPoint.toString());
+		Circle circle = (Circle) joinPoint.getTarget();// the object
+		System.out.println(circle.getName());
 	}
 	
 //	// wildcard: 0 or any number of arguments
@@ -30,8 +36,13 @@ public class LoggingAspect {
 //		System.out.println("Advice3 run. Get Method called.");
 //	}
 	
+	@Before("args(name)")
+	public void stringArgumentMethods(String name) { // name is the actual argument, and we are also telling spring that the argument is of type String
+		System.out.println("A method that takes String arguments has been called. The value is " + name);
+	}
 	
-	@Before("allCircleMethods()")
+	
+	@Before("allGetters()")
 	public void secondAdvice() {
 		System.out.println("Second Advice executed.");
 	}
